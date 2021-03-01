d53be1c1f07263333666643347650e19f5dabb89
anup-paul <anuppaul1845@gmail.com>
2021-03-01 06:59:18 +0600

    remove-console-log

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -2,9 +2,7 @@ import AddPlayer from "../AddPlayer/AddPlayer";
 import './Cart.css'
 
 const Cart = (props) => {
-    // console.log(props.cartInfo)
-    const cart = props.cartInfo
-    // const totalBudget = cart.reduce((initialSalary,addPlayerSalary)=>initialSalary+addPlayerSalary.salary,0)
+    const cart = props.cartInfo;
     let totalBudget = 0;
     for (let i = 0; i < cart.length; i++) {
          const player = cart[i];
--- src/Components/Players/Players.js
+++ src/Components/Players/Players.js
@@ -5,17 +5,16 @@ import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import './Players.css'
 
 const Players = (props) => {
-    // console.log(props.playersInfo)
     const { name, email, salary, country, image } = props.playersInfo;
     const handleAddPlayer = props.handleAddPlayer;
     return (
         <div className="d-flex justify-content-center align-items-center">
             <div className=" player-box-design d-flex justify-content-center align-items-center" >
                 <div className="player-details">
-                    <h5>name:{name}</h5>
-                    <p>email:{email}</p>
-                    <p>salary:${salary}</p>
-                    <p>country:{country}</p>
+                    <h5>Name:{name}</h5>
+                    <p>Email:{email}</p>
+                    <p>Salary:${salary}</p>
+                    <p>Country:{country}</p>
                     <button className="btn btn-dark" onClick={() => handleAddPlayer(props.playersInfo)}> <FontAwesomeIcon icon={faUserPlus} /> add player</button>
                 </div>
                 <div>
